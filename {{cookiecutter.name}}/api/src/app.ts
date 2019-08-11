1d75fd89a584bae0caab2d793c611ebce878c54b
G. Rampaart <g.rampaart@gs-mac-mini.lan>
2019-08-11 17:11:59 +0200

    made sure I can start express locally without the test project

--- {{cookiecutter.name}}/api/src/app.ts
+++ {{cookiecutter.name}}/api/src/app.ts
@@ -3,21 +3,22 @@
 import * as express from "express";
 import { join } from "path";
 import bodyParser from "./bodyParser";
-import { PetsService } from "./services/pet/pet.service";
-import petsSample from "./services/pet/pet.sample";
+import PetsService from "./services/pet/pet.service";
 
 const expressApp = express();
 bodyParser(expressApp);
 
 expressApp.get("/api/pets", async (req: express.Request, res: express.Response) => {
-    res.json(PetsService);
+    const all = await PetsService.all();
+    res.json(all.filter((pet) => 
+    pet.id === req.params.petId));
 });
 
 expressApp.get("/api/pets/:petId", async (req: express.Request, res: express.Response) => {
     res.json(
-        petsSample.filter((pet) => {
-            return pet.id === req.params.petId;
-        })
+        PetsService.byFilter((pet) => 
+            pet.id === req.params.petId;
+        )
     );
 });
 
