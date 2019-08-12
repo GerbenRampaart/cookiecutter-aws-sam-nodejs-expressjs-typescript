ecc61f5f73d53c07360e067b3468b729666fd938
Gerben Rampaart <gerben.rampaart@adp.com>
2019-08-12 17:13:25 +0200

    Working on error handler

--- {{cookiecutter.name}}/api/src/middleware/error.handler.ts
+++ {{cookiecutter.name}}/api/src/middleware/error.handler.ts
@@ -1,7 +1,11 @@
-import { Request, Response,  } from "express";
+import { Request, Response } from "express";
 
 // https://wanago.io/2018/12/17/typescript-express-error-handling-validation/
-const errorHandler = (err: ErrorRequestHandler, req: Request, res: Response) => {
+const errorHandler = (
+  err: any,
+  req: Request,
+  res: Response
+) => {
   const errors = err.errors || [{ message: err.message }];
   res.status(err.status || 500).json({ errors });
 };
