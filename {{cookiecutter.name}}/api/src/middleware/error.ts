68a9e257d0abf8537209c1e0b15395376d98d52b
G. Rampaart <g.rampaart@gs-mac-mini.lan>
2019-08-20 09:09:39 +0200

    error handling

--- {{cookiecutter.name}}/api/src/middleware/error.ts
+++ {{cookiecutter.name}}/api/src/middleware/error.ts
@@ -4,13 +4,8 @@ import { NextFunction } from "connect";
 
 // https://wanago.io/2018/12/17/typescript-express-error-handling-validation/
 const errorMiddleware = (err: HttpException, req: Request, res: Response, next: NextFunction) => {
-  res
-    .status(err.status || 500)
-    .send({
-      isException: true,
-      status: err.status,
-      message: err.message 
-    });
+  res.locals.exception = err;
+  res.status(err.status || 500).end();
 };
 
 export default errorMiddleware;
